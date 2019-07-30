5323af566b0d974f0e7cdde3fdaacbf60fd16d74
ying-li <yinghiu.li18@gmail.com>
2019-07-30 07:44:26 -0700

    added correlation plots to app

--- app.py
+++ app.py
@@ -7,16 +7,32 @@ import pandas as pd
 
 app = Flask(__name__, template_folder="templates")
 
-file = "Resources/Data/combined data/combined_2018.csv"
-df_2018 = pd.read_csv(file)
+file = "Resources/Data/movies.csv"
+df = pd.read_csv(file)
+
+years = {
+    2016 : 1,
+    2017 : 2, 
+    2018 : 3
+}
 
 @app.route("/")
 def index():
     return render_template('index.html')
 
+@app.route("/released_year/<year>")
+def released_year(year):
+    selected_yr = df.loc[df["year"] == int(year)]
+    year_df = json.loads(selected_yr.to_json(orient='records'))
+    return jsonify(year_df)
+
+@app.route("/select_year")
+def select_year():
+    return (jsonify(list(years)))
+
 @app.route("/movies_api")
 def movies_api():
-    data = json.loads(df_2018.to_json(orient='records'))
+    data = json.loads(df.to_json(orient='records'))
     return jsonify(data)
 
 if __name__ == "__main__":
--- data/HOLD.md
+++
@@ -1,1 +0,0 @@
-Hold for dataset.
